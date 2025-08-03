d1d65a886e7c6f1a01fce8f36dfac7581a82a5a5
Hicaro Andre <hicaroandre@hotmail.com>
2025-08-02 22:41:12 -0300

    feat: update hero

--- landinpage/src/App.jsx
+++ landinpage/src/App.jsx
@@ -1,33 +1,37 @@
 // App.jsx
-import { useEffect } from 'react';
-import Hero from './components/Hero.jsx';
-import Portfolio from './components/Portifolio.jsx';
-import Skills from './components/Skills.jsx';
-import About from './components/About.jsx';
-import Footer from './components/Footer.jsx';
-import MyJourney from './components/MyJourney.jsx';
-
+import { useEffect } from "react";
+import ParticlesBackground from "./components/ParticlesBackground";
+import Hero from "./components/Hero.jsx";
+import Portfolio from "./components/Portifolio.jsx";
+import Skills from "./components/Skills.jsx";
+import About from "./components/About.jsx";
+import Footer from "./components/Footer.jsx";
+import MyJourney from "./components/MyJourney.jsx";
 
 function App() {
   useEffect(() => {
     // Tema
-    const switchButton = document.getElementById('switch-button');
+    const switchButton = document.getElementById("switch-button");
     const body = document.body;
 
-    const savedTheme = localStorage.getItem('theme');
-    if (savedTheme === 'light' || (!savedTheme && window.matchMedia('(prefers-color-scheme: light)').matches)) {
-      body.classList.add('light-mode');
+    const savedTheme = localStorage.getItem("theme");
+    if (
+      savedTheme === "light" ||
+      (!savedTheme &&
+        window.matchMedia("(prefers-color-scheme: light)").matches)
+    ) {
+      body.classList.add("light-mode");
       if (switchButton) switchButton.checked = true;
     }
 
     if (switchButton) {
-      switchButton.addEventListener('change', function () {
+      switchButton.addEventListener("change", function () {
         if (this.checked) {
-          body.classList.add('light-mode');
-          localStorage.setItem('theme', 'light');
+          body.classList.add("light-mode");
+          localStorage.setItem("theme", "light");
         } else {
-          body.classList.remove('light-mode');
-          localStorage.setItem('theme', 'dark');
+          body.classList.remove("light-mode");
+          localStorage.setItem("theme", "dark");
         }
       });
     }
@@ -35,32 +39,39 @@ function App() {
     // IntersectionObserver
     const observerOptions = { threshold: 0.2 };
     const observer = new IntersectionObserver((entries) => {
-      entries.forEach(entry => {
+      entries.forEach((entry) => {
         if (entry.isIntersecting) {
-          entry.target.classList.add('visible');
+          entry.target.classList.add("visible");
         }
       });
     }, observerOptions);
 
-    document.querySelectorAll('.animated, .card__skills, .about__text, .about__cta').forEach(el => {
-      observer.observe(el);
-    });
+    document
+      .querySelectorAll(".animated, .card__skills, .about__text, .about__cta")
+      .forEach((el) => {
+        observer.observe(el);
+      });
 
     return () => observer.disconnect();
   }, []);
 
   return (
     <>
-      <Hero />
-      <Portfolio />
-      <Skills />
-      <MyJourney/>
-      <About />
-      <Footer />
-
-
+      <ParticlesBackground />
+      <main>
+        <Hero />
+        <Portfolio />
+        <Skills />
+        <MyJourney />
+        <About />
+        <Footer />
+      </main>
       {/* Botão WhatsApp */}
-      <a href="https://wa.me/98984245018" className="whatsapp-button" target="_blank">
+      <a
+        href="https://wa.me/98984245018"
+        className="whatsapp-button"
+        target="_blank"
+      >
         <img
           className="icon-dark"
           src="src/assets/images/whatsapp-dark.svg"
